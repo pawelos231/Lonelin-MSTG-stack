b1fdc72b96b808c0ae92f88a33279b88c97565d8
pawelLinek <pawellinek3D@gmail.com>
2022-07-30 20:31:58 +0200

    simple data send to serwer

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
+	import type { JSONObject } from '@sveltejs/kit/types/private';
+
 	import { onMount } from 'svelte';
 
 	let test: string[] = [];
 
 	onMount(async function () {
-		const response = await fetch('http://localhost:8080/test');
+		const response: Response = await fetch('http://localhost:8080/test');
 		test = await response.json();
 	});
+	const HandleOnClick = async () => {
+		const obj = {
+			Title: 'siema',
+			CreatedAt: '22002',
+			Message: 'nie wiem',
+			UserName: 'XD'
+		};
+		await fetch('http://localhost:8080/homeland', {
+			method: 'POST',
+			body: JSON.stringify(obj)
+		})
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	};
 </script>
 
 <h1>Combining SvelteKit and Golang server</h1>
 
 <ul>
-	{#each test as t}
-		<li><a href="/subdir/{t}">{t}</a></li>
-	{/each}
+	<button on:click={HandleOnClick}> Kliknij na mnie </button>
 </ul>
--- server/main.go
+++ server/main.go
@@ -1,27 +1,45 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "mime"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"mime"
+	"net/http"
 )
 
 func databases(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    w.Header().Set("Access-Control-Allow-Origin", "*") // for CORS
-    w.WriteHeader(http.StatusOK)
-    test := []string{}
-    test = append(test, "Hello")
-    test = append(test, "World")
-    json.NewEncoder(w).Encode(test)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*") // for CORS
+	w.WriteHeader(http.StatusOK)
+	test := []string{}
+	test = append(test, "Spierdalaj")
+	test = append(test, "Nie")
+	json.NewEncoder(w).Encode(test)
+}
+
+type PostInformiation struct {
+	Title     string `json:"title"`
+	CreatedAt string `json:"createdAt"`
+	Message   string `json:"message"`
+	UserName  string `json:"userName"`
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+	var PostDetails PostInformiation
+	json.NewDecoder(r.Body).Decode(&PostDetails)
+	fmt.Println(PostDetails)
 }
 
 func main() {
-    // Windows may be missing this
-    mime.AddExtensionType(".js", "application/javascript")
 
-    http.Handle("/test", http.HandlerFunc(databases))
-    http.Handle("/", http.FileServer(http.Dir("static")))
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	mime.AddExtensionType(".js", "application/javascript")
+
+	http.Handle("/test", http.HandlerFunc(databases))
+	http.Handle("/homeland", http.HandlerFunc(HomeHandler))
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
