c690afd0ec7f3e7e1f1725b24ea00bf924216f8f
pawelLinek <pawellinek3D@gmail.com>
2022-08-11 16:16:27 +0200

    created schema for comments section

--- server/pkg/models/PostModel.go
+++ server/pkg/models/PostModel.go
@@ -17,3 +17,15 @@ type User struct {
 	Email    string `json:"email"`
 	Password string `json:"Password"`
 }
+
+type Comment struct {
+	UserName    string `json:username`
+	UserId      string `json:userid`
+	CreatedAt   string `json:createdat`
+	PostId      string `json:postid`
+	Likes       string `json:likes`
+	parentId    string `json:parentid`
+	NestedLevel string `json:nestedlevel`
+	Repondsto   string `json:respondsto`
+	updatedAt   string `json:updatedats`
+}
