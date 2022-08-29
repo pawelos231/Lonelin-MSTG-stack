5de349272c604830cebcabc4a108e2dccfea0825
pawelLinek <pawellinek3D@gmail.com>
2022-08-29 22:45:03 +0200

    added created posts frontend view, fix styling later, cause it's horrile

--- client/src/routes/user/CreatedPosts.svelte
+++ client/src/routes/user/CreatedPosts.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import type { UserInfo } from '../../interfaces/UserInfoLogin';
+	import { Card, CardText } from 'svelte-materialify';
 	import { createScene } from '../../scenes/scene';
-
+	import type { PostDetails } from '../../interfaces/PostDetails';
+	let Posts: PostDetails | any = [];
 	onMount(async function () {
 		let ProfileObj: UserInfo | any = JSON.parse(localStorage.getItem('profile') || '{}');
 		console.log(ProfileObj);
@@ -17,7 +19,11 @@
 				body: JSON.stringify(ProfileObj.email)
 			})
 				.then((res) => res.json())
-				.then((data) => console.log(data));
+				.then((data) => {
+					console.log(data);
+					Posts = data;
+					console.log(Posts);
+				});
 		};
 		FetchUselessData();
 	});
@@ -28,7 +34,24 @@
 	});
 </script>
 
-<div class="overflow-hidden flex flex-col justify-center relative mt-0">
+<div class="overflow-hidden flex flex-col justify-center relative mt-0 items-center top-0">
 	<canvas bind:this={el} />
-	<button class="text-5xl absolute top-52 z-50 left-52 text-white">click</button>
+	<h1 class=" absolute text-5xl  text-white top-24">Stworzone Posty</h1>
+	<div class="absolute text-white z-40 top-1/4 flex flex-wrap w-4/6 justify-center gap-2 ">
+		{#each Posts as post}
+			<Card hover outlined style="width:180px; height:30vh; overflow:hidden">
+				<div class="basis-1/4 bg-slate-300 h-full relative rounded-xl">
+					<a sveltekit:prefetch href="/posts/{post._id}">
+						<div class="h-2/4 overflow-hidden">
+							<img class=" top-0 left-0 h-full object-cover w-full" src={post.image} alt="" />
+						</div>
+						<h2 class=" text-left mb-3 pb-3 font-light text-sm">{post.title}</h2>
+						<p class="text-overline text-gray-700 text-xs">
+							Stworzone w: <span class="text-sm">{post.createdat}</span>
+						</p>
+					</a>
+				</div>
+			</Card>
+		{/each}
+	</div>
 </div>
--- client/src/routes/user/__layout.svelte
+++ client/src/routes/user/__layout.svelte
@@ -16,7 +16,7 @@
 
 <main>
 	<nav class="h-[calc(100vh-40px)] absolute opacity-90 z-10 top-10">
-		<ul class="flex flex-col gap-5 bg-gray-800 w-52 h-full pt-5">
+		<ul class="flex flex-col gap-5 bg-gray-800 w-52 min-h-full pt-5">
 			{#each dataOfNavbarTextElements as { text, link }}
 				<LiElement value={text} {link} />
 			{/each}
