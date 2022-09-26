d9695a617215a0305bb52343dafdf636c6a51043
pawelLinek <pawellinek3D@gmail.com>
2022-09-26 22:57:12 +0200

    added check refresh token in middleware, later abstract it to different functions cause it duplicates

--- server/pkg/middleware/auth.go
+++ server/pkg/middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,6 +13,35 @@ import (
 func JwtVerify(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		//later place it in a header, i don't know why but cors keeps complaining when i try to set it
+
+		//MOVE THIS ENTIRE THING TO SOME DIFFERENT FUNCTION CAUSE IT DUPLICATES IN USERCONTROLLER/REFRESHTOKEN
+
+		tokenCookie2, errCookie := req.Cookie("jid")
+		if errCookie != nil {
+			fmt.Println(errCookie)
+			json.NewEncoder(w).Encode("coś poszło nie tak")
+			return
+		}
+
+		value := tokenCookie2.Value
+
+		tkClaims := jwt.MapClaims{}
+		refreshToken, errParsed := jwt.ParseWithClaims(value, tkClaims, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("REFRESH_TOKEN_SECRET")), nil
+		})
+		//Pass it to utils later beacuse it duplicates
+		if errParsed != nil || refreshToken == nil {
+			if errParsed == jwt.ErrSignatureInvalid {
+				json.NewEncoder(w).Encode("invalid token")
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode("zły token")
+			println("blad")
+			return
+		}
+
 		tokenValue := req.FormValue("q")
 		tk := jwt.MapClaims{}
 		tkn, err := jwt.ParseWithClaims(tokenValue, tk, func(token *jwt.Token) (interface{}, error) {
