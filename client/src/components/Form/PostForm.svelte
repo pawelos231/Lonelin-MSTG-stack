4b002716a31dd33e8a73a968787383d1e344c27a
pawelLinek <pawellinek3D@gmail.com>
2022-10-04 16:20:43 +0200

    style fix for post add

--- client/src/components/Form/PostForm.svelte
+++ client/src/components/Form/PostForm.svelte
@@ -16,39 +16,47 @@ items-center z-20  "
 	>
 		X
 	</div>
-	<form
-		on:submit={(e) => HandleOnClick(e)}
-		enctype="multipart/form-data"
-		method="post"
-		class="mb-10 flex flex-col w-1/5 p-4  bg-slate-600 gap-9 rounded z-10 "
-	>
-		<input
-			required
-			class="border-2 rounded border-slate-300 border-dashed h-14"
-			bind:value={Title}
-			type="text"
-			placeholder="wprowadz tytuł"
-		/>
-		<textarea
-			required
-			class="border-2 rounded border-slate-300 border-dashed h-14"
-			bind:value={Message}
-			type="text"
-			placeholder="wprowadz wiadomość"
-		/>
-		<input
-			required
-			name="PostFile"
-			type="file"
-			class="bg-red w-50"
-			accept=".jpg, .jpeg, .png"
-			on:change={(e) => onFileSelected(e)}
-		/>
-		<button
-			class=" bg-slate-800 rounded p-3 cursor-pointer m-3 transition-all ease-in-out duration-300 hover:bg-black hover:text-white "
-			type="submit"
+	<div class="bg-stone-800 w-[40%] h-[50%] h border-2 rounded-sm">
+		<form
+			on:submit={(e) => HandleOnClick(e)}
+			enctype="multipart/form-data"
+			method="post"
+			class=" w-full h-full mb-10 flex flex-col  rounded-sm  p-4   gap-9  z-10  relative"
 		>
-			Stwórz post
-		</button>
-	</form>
+			<div class="border-2 rounded border-stone-500 text-white">
+				<input
+					required
+					class=" outline-none h-14 p-2"
+					bind:value={Title}
+					type="text"
+					placeholder="wprowadz tytuł"
+				/>
+			</div>
+
+			<div class="w-full border-2 rounded border-stone-500 text-white">
+				<textarea
+					required
+					class="0  h-14 w-full outline-none p-2"
+					bind:value={Message}
+					type="text"
+					placeholder="wprowadz wiadomość"
+				/>
+			</div>
+			<div class="flex justify-center">
+				<input
+					required
+					name="PostFile"
+					type="file"
+					class=" w-50"
+					accept=".jpg, .jpeg, .png"
+					on:change={(e) => onFileSelected(e)}
+				/>
+			</div>
+			<div
+				class="bg-stone-800 border-2 border-stone-500 text-white   rounded p-3 cursor-pointer m-3 transition-all ease-in-out duration-100 hover:bg-stone-900 hover:text-white flex justify-center bottom-6 absolute w-[90%]"
+			>
+				<button type="submit"> Stwórz post </button>
+			</div>
+		</form>
+	</div>
 </div>
